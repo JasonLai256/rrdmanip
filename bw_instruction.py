92834cc7b98790c107b263a231a04da8a4dfb300
Jason Lai <jasonlai256@gmail.com>
2012-12-26 15:44:13 +0800

    update RRA

--- bw_instruction.py
+++ bw_instruction.py
@@ -35,7 +35,7 @@ def register_rrd(domain):
     """Add a RRDManip."""
     if domain not in _rrds:
         filepath = figure_path(domain)
-        roundRobinArchives = settings.MAX_RRA
+        roundRobinArchives = settings.MAX_RRA + settings.AVERAGE_RRA
         datasource = settings.DataSource.copy()
         # 取 domain 的右起第二段为 dsName，即 a.myzaker.com -> myzaker
         dsname = domain.rsplit('.', 2)[-2]
--- settings.py
+++ settings.py
@@ -13,12 +13,10 @@ DataSource = {
 }
 
 # rra 的相关设置
-
-# 5 min, 12 * 24 * 365 ~ 1 year
 MAX_5M_1Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
-    'steps': 1,
+    'steps': 1,    # 5 min, 12 * 24 * 365 ~ 1 year
     'rows': 105120
 }
 
@@ -43,4 +41,33 @@ MAX_1D_3Y_RRA = {
     'rows': 1100
 }
 
+AVERAGE_5M_1Y_RRA = {
+    'cf': 'AVERAGE',
+    'xff': 0.5,
+    'steps': 1,    # 5 min, 12 * 24 * 365 ~ 1 year
+    'rows': 105120
+}
+
+AVERAGE_30M_1Y_RRA = {
+    'cf': 'AVERAGE',
+    'xff': 0.5,
+    'steps': 6,    # 30 min, 2 * 24 * 365 ~ 1 year 
+    'rows': 17520
+}
+
+AVERAGE_2H_1Y_RRA = {
+    'cf': 'AVERAGE',
+    'xff': 0.5,
+    'steps': 24,    # 2 hour, 12 * 365 ~ 1 year
+    'rows': 4380
+}
+
+AVERAGE_1D_3Y_RRA = {
+    'cf': 'AVERAGE',
+    'xff': 0.5,
+    'steps': 288,    # 1 day, 365 * 3 ~ 3 year
+    'rows': 1100
+}
+
 MAX_RRA = [MAX_5M_1Y_RRA, MAX_30M_1Y_RRA, MAX_2H_1Y_RRA, MAX_1D_3Y_RRA]
+AVERAGE_RRA = [AVERAGE_5M_1Y_RRA, AVERAGE_30M_1Y_RRA, AVERAGE_2H_1Y_RRA, AVERAGE_1D_3Y_RRA]
