f9fdbc5fd5fc132aefc39038775c5ff69e52c181
Jason Lai <jasonlai256@gmail.com>
2012-12-28 15:57:48 +0800

    修改rra以做测试。

--- settings.py
+++ settings.py
@@ -13,12 +13,34 @@ DataSource = {
 }
 
 # rra 的相关设置
-MAX_1M_1Y_RRA = {
-    'cf': 'MAX',
+MIN_5M_1Y_RRA = {
+    'cf': 'MIN',
     'xff': 0.5,
-    'steps': 1,
+    'steps': 5,    # 5 min, 12 * 24 * 365 ~ 1 year
     'rows': 105120
 }
+
+MIN_30M_1Y_RRA = {
+    'cf': 'MIN',
+    'xff': 0.5,
+    'steps': 30,    # 30 min, 2 * 24 * 365 ~ 1 year 
+    'rows': 17520
+}
+
+MIN_2H_1Y_RRA = {
+    'cf': 'MIN',
+    'xff': 0.5,
+    'steps': 120,    # 2 hour, 12 * 365 ~ 1 year
+    'rows': 4380
+}
+
+MIN_1D_3Y_RRA = {
+    'cf': 'MIN',
+    'xff': 0.5,
+    'steps': 1440,    # 1 day, 365 * 3 ~ 3 year
+    'rows': 1100
+}
+
 
 MAX_5M_1Y_RRA = {
     'cf': 'MAX',
@@ -46,13 +68,6 @@ MAX_1D_3Y_RRA = {
     'xff': 0.5,
     'steps': 1440,    # 1 day, 365 * 3 ~ 3 year
     'rows': 1100
-}
-
-AVERAGE_1M_1Y_RRA = {
-    'cf': 'AVERAGE',
-    'xff': 0.5,
-    'steps': 1,
-    'rows': 105120
 }
 
 AVERAGE_5M_1Y_RRA = {
@@ -83,5 +98,6 @@ AVERAGE_1D_3Y_RRA = {
     'rows': 1100
 }
 
-MAX_RRA = [MAX_1M_1Y_RRA, MAX_5M_1Y_RRA, MAX_30M_1Y_RRA, MAX_2H_1Y_RRA, MAX_1D_3Y_RRA]
-AVERAGE_RRA = [AVERAGE_1M_1Y_RRA, AVERAGE_5M_1Y_RRA, AVERAGE_30M_1Y_RRA, AVERAGE_2H_1Y_RRA, AVERAGE_1D_3Y_RRA]
+MIN_RRA = [MIN_5M_1Y_RRA, MIN_30M_1Y_RRA, MIN_2H_1Y_RRA, MIN_1D_3Y_RRA]
+MAX_RRA = [MAX_5M_1Y_RRA, MAX_30M_1Y_RRA, MAX_2H_1Y_RRA, MAX_1D_3Y_RRA]
+AVERAGE_RRA = [AVERAGE_5M_1Y_RRA, AVERAGE_30M_1Y_RRA, AVERAGE_2H_1Y_RRA, AVERAGE_1D_3Y_RRA]
