5eb764ec3c2358420b5252ee85450563f0a4c6e8
Jason Lai <jasonlai256@gmail.com>
2012-12-28 16:52:46 +0800

    最终确定rra。

--- settings.py
+++ settings.py
@@ -13,35 +13,6 @@ DataSource = {
 }
 
 # rra 的相关设置
-MIN_5M_1Y_RRA = {
-    'cf': 'MIN',
-    'xff': 0.5,
-    'steps': 5,    # 5 min, 12 * 24 * 365 ~ 1 year
-    'rows': 105120
-}
-
-MIN_30M_1Y_RRA = {
-    'cf': 'MIN',
-    'xff': 0.5,
-    'steps': 30,    # 30 min, 2 * 24 * 365 ~ 1 year 
-    'rows': 17520
-}
-
-MIN_2H_1Y_RRA = {
-    'cf': 'MIN',
-    'xff': 0.5,
-    'steps': 120,    # 2 hour, 12 * 365 ~ 1 year
-    'rows': 4380
-}
-
-MIN_1D_3Y_RRA = {
-    'cf': 'MIN',
-    'xff': 0.5,
-    'steps': 1440,    # 1 day, 365 * 3 ~ 3 year
-    'rows': 1100
-}
-
-
 MAX_5M_1Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
@@ -98,7 +69,5 @@ AVERAGE_1D_3Y_RRA = {
     'rows': 1100
 }
 
-MIN_RRA = [MIN_5M_1Y_RRA, MIN_30M_1Y_RRA, MIN_2H_1Y_RRA, MIN_1D_3Y_RRA]
 MAX_RRA = [MAX_5M_1Y_RRA, MAX_30M_1Y_RRA, MAX_2H_1Y_RRA, MAX_1D_3Y_RRA]
-MAX_RRA += MIN_RRA
 AVERAGE_RRA = [AVERAGE_5M_1Y_RRA, AVERAGE_30M_1Y_RRA, AVERAGE_2H_1Y_RRA, AVERAGE_1D_3Y_RRA]
