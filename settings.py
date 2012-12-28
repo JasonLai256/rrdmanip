a5340b534effc4600dd302b7f5377d5a9ade9d6b
Jason Lai <jasonlai256@gmail.com>
2012-12-28 13:41:23 +0800

    修改rrs

--- settings.py
+++ settings.py
@@ -3,69 +3,69 @@
 
 DATA_PATH = '/home/data/rrd-storage/'
 
-STEP = 300
+STEP = 60
 
 # data source 的相关设置
 DataSource = {
     'dsName': '',
     'dsType': 'GAUGE',
-    'heartbeat': 600
+    'heartbeat': 120
 }
 
 # rra 的相关设置
 MAX_5M_1Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
-    'steps': 1,    # 5 min, 12 * 24 * 365 ~ 1 year
+    'steps': 5,    # 5 min, 12 * 24 * 365 ~ 1 year
     'rows': 105120
 }
 
 MAX_30M_1Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
-    'steps': 6,    # 30 min, 2 * 24 * 365 ~ 1 year 
+    'steps': 30,    # 30 min, 2 * 24 * 365 ~ 1 year 
     'rows': 17520
 }
 
 MAX_2H_1Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
-    'steps': 24,    # 2 hour, 12 * 365 ~ 1 year
+    'steps': 120,    # 2 hour, 12 * 365 ~ 1 year
     'rows': 4380
 }
 
 MAX_1D_3Y_RRA = {
     'cf': 'MAX',
     'xff': 0.5,
-    'steps': 288,    # 1 day, 365 * 3 ~ 3 year
+    'steps': 1440,    # 1 day, 365 * 3 ~ 3 year
     'rows': 1100
 }
 
 AVERAGE_5M_1Y_RRA = {
     'cf': 'AVERAGE',
     'xff': 0.5,
-    'steps': 1,    # 5 min, 12 * 24 * 365 ~ 1 year
+    'steps': 5,    # 5 min, 12 * 24 * 365 ~ 1 year
     'rows': 105120
 }
 
 AVERAGE_30M_1Y_RRA = {
     'cf': 'AVERAGE',
     'xff': 0.5,
-    'steps': 6,    # 30 min, 2 * 24 * 365 ~ 1 year 
+    'steps': 30,    # 30 min, 2 * 24 * 365 ~ 1 year 
     'rows': 17520
 }
 
 AVERAGE_2H_1Y_RRA = {
     'cf': 'AVERAGE',
     'xff': 0.5,
-    'steps': 24,    # 2 hour, 12 * 365 ~ 1 year
+    'steps': 120,    # 2 hour, 12 * 365 ~ 1 year
     'rows': 4380
 }
 
 AVERAGE_1D_3Y_RRA = {
     'cf': 'AVERAGE',
     'xff': 0.5,
-    'steps': 288,    # 1 day, 365 * 3 ~ 3 year
+    'steps': 1440,    # 1 day, 365 * 3 ~ 3 year
     'rows': 1100
 }
 
